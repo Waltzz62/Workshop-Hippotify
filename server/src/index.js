b16f0c4efd3cbd493dd413b5e45417519b7025d4
Worapratch <worapratch06@gmail.com>
2025-01-27 16:31:08 +0700

    db connection added

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import connection from "./config/database.js";
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+connection.connect((err)=>{
+    if(err) {
+        console.log(err);
+    }else {
+        console.log("database is connected");
+    }
+    
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    
+})
